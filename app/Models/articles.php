2924e8f612487bda023b8224efb10fd2170f4e62
Moh Farog <eldgeel2000@gmail.com>
2022-05-17 19:40:15 +0400

    Updates

--- app/Models/articles.php
+++ app/Models/articles.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class articlesTable extends Model
+class articles extends Model
 {
     use HasFactory;
 }
--- database/migrations/2022_05_17_153844_create_articles_table.php
+++ database/migrations/2022_05_17_153844_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTablesTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateArticlesTablesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles_tables', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id')->unsigned()->nullable();
             $table->string('ar_title');
@@ -34,6 +34,6 @@ class CreateArticlesTablesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles_tables');
+        Schema::dropIfExists('articles');
     }
 }
