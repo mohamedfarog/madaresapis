3d79b079bc48eaf9da71f072480c62a574a97795
Moh Farog <eldgeel2000@gmail.com>
2022-06-22 20:34:45 +0400

    lastUpated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -58,7 +58,7 @@ class RegisterController extends Controller
         
                 $userType->user_type = $request->type;
                 $userType->save();
-                if ($userType->user_type  == 255) {
+                if ($userType->user_type  === '255') {
                
                     $userId = $request->id;
                      $academy = new Academy();
@@ -147,7 +147,7 @@ class RegisterController extends Controller
                 else{
                     return "Noooo";
                 }
-                if ($request->type == 256) {
+                if ($request->type === '256') {
                     $userId = $request->id;
             
                     $teacher = new Teacher();
