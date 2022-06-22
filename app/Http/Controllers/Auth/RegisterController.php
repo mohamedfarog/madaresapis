1b0ed17b40b5dc396efaf84ec4fe4a72596678ae
Moh Farog <eldgeel2000@gmail.com>
2022-06-22 20:18:48 +0400

    lastt

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -119,7 +119,7 @@ class RegisterController extends Controller
                     $location->save();
                     
                     if(asset($request->academy_levels )){
-                        return $request->academy_levels;
+                        return "Hello";
                     }
                     foreach ($request->academy_levels as $level) {
                       
