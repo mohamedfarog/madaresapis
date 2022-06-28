cbf3570b004b54e4404992510b99398e1a6294cc
Moh Farog <eldgeel2000@gmail.com>
2022-06-28 19:02:06 +0400

    update

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -22,7 +22,8 @@ class ForgotPasswordController extends Controller
        'email' => 'required|email|exists:users',
    ]);
 
-     $token = Str::random(40);
+     $token = Str::random(64);
+    
      DB::table('password_resets')->insert(
          ['email' => $request->email, 'token' => $token, 'created_at' => Carbon::now()]
      );
@@ -32,6 +33,6 @@ class ForgotPasswordController extends Controller
          $message->subject('Reset Password Notification');
      });
 
-     return $this->onSuccess("Reset password link sent");
+     return $this->onSuccess(["Reset password link sent", $token]);
  }
 }
