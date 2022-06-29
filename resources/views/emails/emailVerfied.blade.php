665e266fdf2135b2c77c185c96d9df7a0ba93098
Moh Farog <eldgeel2000@gmail.com>
2022-06-29 18:43:56 +0400

    updateEmail

--- resources/views/emails/emailVerfied.blade.php
+++ resources/views/emails/emailVerfied.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="icon" href="logo512.png">
+    <link rel="icon" href="/logo512.png">
     <title class="tr">Madares</title>
 	<script>
 		window.addEventListener('load', (event) => {
