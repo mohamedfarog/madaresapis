ecfe6f9c9d1e653be2f1fb2f3bdf428a6f6392d7
Moh Farog <eldgeel2000@gmail.com>
2022-07-04 18:57:10 +0400

    updateAbout

--- database/migrations/2022_07_04_145212_add_ar_title_to_abouts_table.php
+++ database/migrations/2022_07_04_145212_add_ar_title_to_abouts_table.php
@@ -14,7 +14,8 @@ class AddArTitleToAboutsTable extends Migration
     public function up()
     {
         Schema::table('abouts', function (Blueprint $table) {
-            //
+            $table->string('ar_title');
+
         });
     }
 
@@ -26,7 +27,6 @@ class AddArTitleToAboutsTable extends Migration
     public function down()
     {
         Schema::table('abouts', function (Blueprint $table) {
-            $table->string('ar_title');
         });
     }
 }
