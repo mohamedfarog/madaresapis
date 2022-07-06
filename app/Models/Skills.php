b38169513250a9d0e4346e20e0625ba00b56101b
Moh Farog <eldgeel2000@gmail.com>
2022-07-06 16:58:10 +0400

    updated

--- app/Models/Skills.php
+++ app/Models/Skills.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Skills extends Model
 {
     use HasFactory;
-    protected $fillable = ['teacher_id','skill_id'];
+    protected $fillable = ['teacher_id','kill_id'];
     protected $guarded = ['id'];
     protected $hidden = ['updated_at', 'created_at'];
 }
