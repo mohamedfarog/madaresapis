ae33078c41e1683eaa65017b502071f275b2285a
Moh Farog <eldgeel2000@gmail.com>
2022-07-15 16:08:16 +0400

    added jobs state

---
+++ database/migrations/2022_07_15_115607_add_state_to_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStateToJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->dropColumn('advertise_area');
+            $table->string('country')->default('Saudi Arabia');
+            $table->string('state')->default('Al Riyadh');
+            $table->tinyInteger('language')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->string('advertise_area');
+            $table->dropColumn('country');
+            $table->dropColumn('state');
+            $table->dropColumn('language');
+
+        });
+    }
+}
