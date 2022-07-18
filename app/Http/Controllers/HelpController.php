1e94ad936d3bb390f71d52b04f66e91edddd2637
faisal <faisal@mvp-apps.ae>
2022-07-18 15:38:32 +0400

    added help api

---
+++ app/Http/Controllers/HelpController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\HelpAcknowledgement;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class HelpController extends Controller
+{
+    public function sendHelpRequest(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            "email" => "required|email",
+            "subject" => "required",
+            "mobile" => "required",
+            "body" => "required",
+        ]);
+
+        if ($validator->fails()) {
+            return $this->onError($validator->errors()->all());
+        }
+        Mail::to($request->email)->cc(['faisal@mvp-apps.ae'])->send(new HelpAcknowledgement());
+        return  $this->onSuccess(['message' => "Request Send Successfully"]);
+    }
+}
