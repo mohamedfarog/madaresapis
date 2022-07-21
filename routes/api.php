9a96fa62d4c27e2e609599efd3c4d2f773ec5be6
faisal <faisal@mvp-apps.ae>
2022-07-21 18:33:44 +0400

    udapted to get

--- routes/api.php
+++ routes/api.php
@@ -126,8 +126,8 @@ Route::group(['middleware' => 'auth:api'], function () {
    Route::post('userType', [RegisterController::class, 'UpdateUserType']);
    Route::get('my_info', [AuthController::class, 'my_info']);
    Route::post('send_help_request', [HelpController::class, 'sendHelpRequest']);
-   Route::post('search_teachers', [RegisterController::class, 'search_teachers']);
-   Route::post('teacher_info', [RegisterController::class, 'teacher_info']);
+   Route::get('search_teachers', [RegisterController::class, 'search_teachers']);
+   Route::get('teacher_info', [RegisterController::class, 'teacher_info']);
 
 
    Route::post('years_of_teaching', [AcademyController::class, 'academyYearsOfTeaching']);
