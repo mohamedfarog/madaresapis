c2fc06a030352717694512d4c2fd2cd0d88679cf
faisal <faisal@mvp-apps.ae>
2022-07-21 09:51:10 +0400

    added middleware

--- app/Http/Controllers/Job/JobController.php
+++ app/Http/Controllers/Job/JobController.php
@@ -96,9 +96,7 @@ class JobController extends Controller
 
    public function get_my_jobs(Request $request)
    {
-      if(isset($request->lng)&&$request->lng==1){
-         App::setLocale('ar');
-      }
+      
       $academy = Academy::where('user_id', Auth::id())->first();
       $data = Job::with(['academy', 'level', 'type', 'subjects'])->withCount(['applications', 'awaiting', 'reviewed', 'contacting', 'rejected'])->where("academy_id", $academy->id)->whereNull('deleted_at')->paginate();
       return $this->onSuccess($data);
