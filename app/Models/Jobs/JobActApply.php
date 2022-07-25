cac7346b2681bb0273271e1e923da711b90e5697
Big Mo <mohammednasir240199@gmail.com>
2022-07-25 13:31:58 +0400

    Removed Academy from the getAllApplications API

--- app/Models/Jobs/JobActApply.php
+++ app/Models/Jobs/JobActApply.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class JobActApply extends Model
 {
     protected $table = 'job_act_apply';
+    protected $appends = ['job_status'];
     use HasFactory;
 
     public function academy(): BelongsTo
@@ -25,5 +26,38 @@ class JobActApply extends Model
     public function teacher(): BelongsTo
     {
         return $this->belongsTo(Teacher::class, "teacher_id", 'id');
+    }
+    public function getJobStatusAttribute()
+    {
+
+        switch ($this->status) {
+            case 0:
+                return 'Pending';
+                break;
+            case 1:
+                return 'Viewed';
+                break;
+            case 2:
+                return 'Shortlisted';
+                break;
+            case 3:
+                return 'Rejected';
+                break;
+            case 4:
+                return 'Contacted';
+                break;
+            case 5:
+                return 'Interviewed';
+                break;
+            case 6:
+                return 'On Hold';
+                break;
+            case 7:
+                return 'Accepted';
+                break;
+            default:
+                return 'Awaiting';
+                break;
+        }
     }
 }
