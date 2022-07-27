78b4669beafa3ca66d767c74b63bd8df37c19a07
Moh Farog <eldgeel2000@gmail.com>
2022-07-27 15:21:28 +0400

    dgeel

--- app/Http/Controllers/Job/JobController.php
+++ app/Http/Controllers/Job/JobController.php
@@ -151,20 +151,19 @@ class JobController extends Controller
    }
    public function getAllApplications(Request $request)
    {
-      // $validator = Validator::make($request->all(), [
-      //    'status' => ['nullable', Rule::in([0, 1, 2, 3, 4, 5, 6,7,8]),],
-      // ]);
-      // if ($validator->fails()) {
-      //    return $this->onError($validator->errors()->all());
-      // }
+      $validator = Validator::make($request->all(), [
+         'status' => ['nullable', Rule::in([0, 1, 2, 3, 4, 5,7,8]),],
+      ]);
+      if ($validator->fails()) {
+         return $this->onError($validator->errors()->all());
+      }
       $academy = Academy::where('user_id', Auth::id())->first();
       if (!$academy) {
          return $this->onError(["No Academy Found"]);
       }
       $whereDeleted =  Job::whereNotNull('deleted_at')->where('academy_id', $academy->id)->get()->pluck('id');
       $jobApply = JobActApply::where('academy_id', $academy->id)->whereNotIn('academy_id', $whereDeleted)->with(['jobs', 'teacher']);
-      // $jobApply = JobActApply::where('academy_id', $academy->id)->with(['jobs', 'teacher']);
-      if (isset($request->status)) {
+            if (isset($request->status)) {
          $jobApply = $jobApply->where('status', $request->status);
       }
       $jobApply = $jobApply->paginate();
