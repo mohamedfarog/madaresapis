c8fdaccd317bc35b5e40abe1e47558e6d7929eed
Moh Farog <eldgeel2000@gmail.com>
2022-07-28 09:59:37 +0400

    dgeeel

--- app/Http/Controllers/Website/AcademyController.php
+++ app/Http/Controllers/Website/AcademyController.php
@@ -48,7 +48,7 @@ public function deleteAcademyFile(Request $request){
             return $this->onError('Academy File does not exist');
         }
         if($file = $request->file_url){
-               $academy_file = $this->uploadFile($file, 'file_url');
+               $academy_file = $this->uploadFile($file, 'academyFiles');
                $academyFile->file_url = $request->file_url;
         }
         $academyFile->save();
