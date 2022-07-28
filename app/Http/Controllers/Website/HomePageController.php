87477dd10a0f595aa8fb29b9cf6eea4c921e23af
hussam.alhennawi <alhennawi.hussam@gmail.com>
2022-07-28 12:35:48 +0400

    - update (Teacher -> search jobs & applications (new route --> teacherApplications)).

--- app/Http/Controllers/Website/HomePageController.php
+++ app/Http/Controllers/Website/HomePageController.php
@@ -113,8 +113,6 @@ public function homePageBanner(Request $request)
 //        $jobs = Job::with('academy', 'level', 'type', 'subjects', 'gender', 'jobMinimumExperience', 'salaryRate')
         $jobs = Job::with('academy', 'level', 'type', 'subjects', 'jobMinimumExperience', 'salaryRate')
             ->where('status', 1)->whereNull('deleted_at')->get();
-//        $jobs=Job::all();
-        dd($jobs);
         return $this->onSuccess($jobs);
     }
 
